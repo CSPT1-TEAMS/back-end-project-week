4e5b57066f1428c2ff7f009e2744d114913c976f
unknown <alexisreyes90@gmail.com>
2018-07-16 22:39:55 -0400

    error fixing for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const port = process.env.PORT || 5050;
 
 express.get('/', (req, res) => {
-    res.json(" !=== Connected ===! ")
+    res.json({ Connected : " !=== Connected ===! " })
 })
 .listen(port, () => {
     console.log(`Listening on ${port}`);
