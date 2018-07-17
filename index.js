0aca6cf9599de86ab86a54f175ffa06a38dc888d
ubuntu <irishseanvaldivia@gmail.com>
2018-07-16 21:52:00 -0500

    Hope this works...

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const port = process.env.PORT || 5050;
 
-express.get('/', (req, res) => {
-  res.json({message: "connected like a boss!"})
-})
-.listen(port, (req, res) => {
-  console.log(`Listening on port ${port}`)
-})
+express()
+  .get("/", (req, res) => {
+    res.json({ message: "connected like a boss!" });
+  })
+  .listen(port, (req, res) => {
+    console.log(`Listening on port ${port}`);
+  });
