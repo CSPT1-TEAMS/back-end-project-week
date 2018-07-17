7e7baf7aa33cccc9b282ef188bf18f66a4d77e5a
unknown <alexisreyes90@gmail.com>
2018-07-16 22:47:42 -0400

    debugging v2

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+
+express
+    .get('/', (req, res) => {
+        res.json({ Connected : "!=== Connected ===!" })
+    })
+    .listen(PORT, () => {
+        console.log(`Listening on ${PORT}`)
+    })
