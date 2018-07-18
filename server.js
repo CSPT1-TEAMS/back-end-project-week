c31d4c417f34b40d60322a2c07534eec623f4cc9
Bhavik Ravani <ravanibhavik@gmail.com>
2018-07-17 22:27:47 -0400

    Connected to mongo dataabse on mlab and added Users and Notes Models.

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const server = express()
 const PORT = process.env.PORT || 5000
+const mongoose = require('mongoose')
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
+mongoose.connect("mongodb://ravanibhavik:asdfgf1234@ds239911.mlab.com:39911/backend-project")
+    .then(() => {
+        console.log("=== Connected to Database ===")
+        server.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}`)
+        })
+    })
+    .catch((error) => {
+        console.log(">>> Error Connecting to Database <<<")
+    })
 
 server.get("/", (req, res) => {
     return res.status(200).json({hello: 'world'})
