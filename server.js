ff318a0d517df776b6cb17e9154af1710d75c5fc
Bhavik Ravani <ravanibhavik@gmail.com>
2018-07-17 22:45:32 -0400

    Added POST method for adding notes.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const server = express()
 const PORT = process.env.PORT || 5000
 const mongoose = require('mongoose')
+const Note = require('./models/Notes')
+
+server.use(express.json())
 
 mongoose.connect("mongodb://ravanibhavik:asdfgf1234@ds239911.mlab.com:39911/backend-project")
     .then(() => {
@@ -17,3 +20,15 @@ mongoose.connect("mongodb://ravanibhavik:asdfgf1234@ds239911.mlab.com:39911/back
 server.get("/", (req, res) => {
     return res.status(200).json({hello: 'world'})
 })
+
+server.post("/notes/new", (req, res) => {
+    Note.create(req.body)
+        .then(newNote => {
+            res.status(201).json(newNote)
+        })
+        .catch(error => {
+            res.status(500).json({errorMessage: 'Unable to add note.'})
+        })
+})
+
+
