c6bce55b583d8934b8b49d3ee8ff3e0943a6358a
unknown <alexisreyes90@gmail.com>
2018-07-19 19:34:02 -0400

    put and delete functional with each specific user/ note

--- Routes/noteRoutes.js
+++ Routes/noteRoutes.js
@@ -13,7 +13,7 @@ router.get('/notes', (req, res) => {
 })
     .post('/notes/new', async (req, res) => {
 
-        const user = await User.findById(/* user token */)
+        const user = await User.findById('5b511eff92daed32d861d0db')
         const createdNote = await Note.create(req.body)
         user.notes.push(createdNote)
         await user.save()
@@ -36,6 +36,38 @@ router.get('/notes', (req, res) => {
                     return res.status(404).json({ Error: "No such note exists." });
                 } else {
                     res.status(200).json(foundNote);
+                }
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
+    })
+
+    .put('/notes/edit/:id', (req, res) => {
+        const { id } = req.params;
+        const { title, content } = req.body;
+
+        Note.findByIdAndUpdate(id, req.body)
+            .then(note => {
+                if(note === null) {
+                    return res.status(404).json({ Error: "No such note exists to edit." });
+                } else {
+                    res.status(200).json(note);
+                }
+            })
+            .catch(err => {
+                res.status(500).json(err);
+            })
+    })
+
+    .delete('/notes/remove/:id', (req, res) => {
+        const { id } = req.params;
+        Note.findByIdAndRemove(id)
+            .then(note => {
+                if(note === null) {
+                    return res.status(404).json({ Error: "No such note exists to delete." });
+                } else {
+                    res.status(200).json(note);
                 }
             })
             .catch(err => {
