e6ed4849e3a976616b17891b2b8d95b871533471
JPEmery <clearkeyemery@gmail.com>
2018-07-21 12:13:28 -0500

    JP Emery: building routes

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
 const express = require('express')
 const path = require('path')
+const helmet = require('helmit');
 const PORT = process.env.PORT || 5000
 const cors = require('cors');
+const mongoose = require('mongoose');
 //declare routes used
 
-const server = express()
+mongoose.connect('mongodb://localhost:27017/notesdb')
+.then()
+.catch(err => {
+  return error;
+})
 
-server.cors()
+const notesController = require('./notes/notesController');
+
+const server = express()
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
 
 server
-  .use(express.static(path.join(__dirname, 'public'))
+  // .use(express.static(path.join(__dirname, 'public'))
   
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
   .get('/', (req, res) => 
   res.json({ hello: "world"})
   
   // res.render('pages/index')
 )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+server.use('/api/', notesController);
+  server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
