4269ae561058b4795bbef047fa62de752e7b4b1a
darthMinimus <irishseanvaldivia@gmail.com>
2018-07-25 21:06:16 -0500

    Let's see if no error without return.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -53,7 +53,7 @@ server.put('/edit/:id', (req, res) => {
     User.findByIdAndUpdate(id, changes, options)
         .then(user => {
             if (note) {
-                return res.status(200).json(user);
+                res.status(200).json(user);
             } else {
                 res.status(404).json({ message: 'User not found' });
             }
