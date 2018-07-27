65ada8c872e8c171c0474afa4750a02f9561beb6
Charlie <ccariste@gmail.com>
2018-07-26 17:35:28 -0700

    CRC: passing id in put

--- routes/notes.js
+++ routes/notes.js
@@ -45,7 +45,7 @@ router.route('/:id')
         if (note === null) {
           return res.status(404).json({msg: 'Note with specified ID does not exist'})
         }
-        res.status(200).json(req.body)
+        res.status(200).json({...req.body, id})
       })
       .catch(error => {
         res.status(500).json(error);
