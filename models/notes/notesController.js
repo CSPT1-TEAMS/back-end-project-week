842ad51ce8febdc29c4ace31d7b5894bbe7bd3bb
unknown <jemery89@gmail.com>
2019-10-27 16:19:39 -0500

    modified id to be ObjectID

--- models/notes/notesController.js
+++ models/notes/notesController.js
@@ -12,20 +12,20 @@ router.route('/')
         console.log(Notes.length)
         Notes.find()
             .then(notes => {
-                if (notes.length === 0) {
+                // if (notes.length === 0) {
 
-                    const testNote =
-                    {
-                        "username": "Mr.T",
-                        "title": "testNote",
-                        "content": "long form content"
-                    }
-                    //const createNote = new Notes(testNote);
-                    Notes.create(testNote).then(note => {
-                        res.status(200).json(note)
-                    });
-                    return
-                }
+                //     const testNote =
+                //     {
+                //         "username": "Mr.T",
+                //         "title": "testNote",
+                //         "content": "long form content"
+                //     }
+                //     //const createNote = new Notes(testNote);
+                //     Notes.create(testNote).then(note => {
+                //         res.status(200).json(note)
+                //     });
+                //     return
+                // }
                 res.status(200).json(notes)
             })
             .catch(err => {
@@ -97,7 +97,7 @@ router.route('/note/:id/:MDBID')
         }
         
         const { title, content } = req.body;
-        Notes.updateOne(id, {$set:{'title': req.body.title, 'content': req.body.content}})
+        Notes.updateOne({id}, {$set:{'title': req.body.title, 'content': req.body.content}})
             .then(note => {
                 res.status(201).json(note)
             })
