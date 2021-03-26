baa831318b79d15c58a971bb46f0759c09140f79
Siddartha08 <jemery89@gmail.com>
2021-03-25 23:41:44 -0500

    adjustment to database call

--- models/notes/notesController.js
+++ models/notes/notesController.js
@@ -12,20 +12,20 @@ router.route('/')
         console.log(Notes.length)
         Notes.find()
             .then(notes => {
-                if (notes.length === 0) {
+                // if (notes.length === 0) {
 
-                    const testNote =
-                    {
-                        "username": "Mr.T",
-                        "title": "testNote",
-                        "content": "long form content"
-                    }
-                    //const createNote = new Notes(testNote);
-                    Notes.create(testNote).then(note => {
-                        res.status(200).json(note)
-                    });
-                    return
-                }
+                //     const testNote =
+                //     {
+                //         "username": "Mr.T",
+                //         "title": "testNote",
+                //         "content": "long form content"
+                //     }
+                //     //const createNote = new Notes(testNote);
+                //     Notes.create(testNote).then(note => {
+                //         res.status(200).json(note)
+                //     });
+                //     return
+                // }
                 res.status(200).json(notes)
             })
             .catch(err => {
@@ -44,8 +44,6 @@ router.route('/')
             .catch(err => {
                 res.status(500).json({ err })
             })
-
-//problem with crashing
 
 
 
